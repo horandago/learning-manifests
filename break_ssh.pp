1b3cce86e0fbd711bf57f5df58b770b40a290f54
Terry Bates <terryjbates@gmail.com>
2013-07-02 09:34:41 +0000

    Created first version of break_ssh.pp.

---
+++ break_ssh.pp
@@ -0,0 +1,12 @@
+file {'/etc/ssh/sshd_config':
+  ensure => file,
+  mode => 600,
+  source => '/root/learning-manifests/sshd_config',
+}
+
+service {'sshd':
+  ensure => running,
+  enable => true,
+  subscribe => File['/etc/ssh/sshd_config'],
+
+}
